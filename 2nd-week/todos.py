5f48341d73ed2d107c68b20db3ce8a9cce729e59
EmbraceLife <3509763625@qq.com>
2024-08-15 18:42:36 +0800

    2nd from scratch on Jeremy's getting started video tutorial: done

--- 2nd-week/todos.py
+++ 2nd-week/todos.py
@@ -1,38 +1,66 @@
 from fasthtml.common import *
 
-app, rt, todos, Todo = fast_app('todos1.db', live=True,      
+def render(todo): # it is added to Todo class and auto render every instance of Todo
+
+    return Li(A('toggle', 
+                hx_get=f'/todo-{todo.id}',
+                target_id=f'todo-{todo.id}', 
+                hx_swap='outerHTML'), 
+
+              A('delete',
+                hx_delete=f'/todo-{todo.id}', # give each todo a unique url for deleting
+                target_id=f'todo-{todo.id}', # give response to the A tag but the Li element
+                hx_swap='outerHTML', # replace the whole element with the response
+                
+                ),
+
+              todo.title + (' ✅' if todo.done else ''),
+
+              id=f'todo-{todo.id}') 
+
+app, rt, todos, Todo = fast_app('todos1.db', live=True, render=render,      
                                 id=int,
                                 title=str,
                                 done=bool,
                                 pk='id')
 
+@rt('/todo-{todoId}')
+def delete(todoId:int):
+    todos.delete(todoId)
+    # return # no return is needed
+
+@rt('/todo-{todoId}') 
+def get(todoId:int):
+    todo = todos.get(todoId)
+    todo.done = not todo.done
+    return todos.update(todo)
+
+
 @rt('/')
 def get():
     out = Titled("Todos",
                     Form(Group(
-                        Input(placeholder='type your todo', name='title', 
-                              id='input', # id is for hx-swap-oob
-                              hx_swap_oob="true"), # going to where the input is, not to the target_id
-                        Button('Add', id='input', # so, the input can replace button as long as it has the same id and hx-swap-oob
-                              hx_swap_oob="true")), 
+                        Input(placeholder='type your todo', name='title', id='user-input', hx_swap_oob="true"),
+                        Button('Add')),
                         hx_post='/getInputData',
                         target_id='todo-list',
                         hx_swap='beforeend'),
-                    Ul(*[Li(t) for t in todos()],
+                    # Ul(*[Li(t) for t in todos()], # each todo is already rendered as a Li
+                    Ul(*todos(), # now todos() is a generator of multiple todo which auto renders as Li already
                         id='todo-list') 
-                 )
+                    )
+                 
     return out
 
 @rt('/getInputData')
 def post(todo:Todo):
-    return (todos.insert(todo),
-            Input(placeholder='I am reborn', name='title', # going to replace itself which is different from target_id
-                  id='input', # id is for hx-swap-oob
-                  hx_swap_oob="true")) # going to where the input is, not to the target_id 
+    return (todos.insert(todo), 
+            Input(placeholder='add a todo here', name='title', id='user-input', hx_swap_oob="true"))
 
 # cli = TestClient(app)
-# pprint(cli.post('/getInputData', data={'title':'put in the end'}).text) 
-# pprint(cli.get('/').text) # same to what displayed in the browser
+# hxhdr = {'headers':{'hx-request':"1"}} # mock to remove the headers
+# # pprint(cli.post('/getInputData', data={'title':'put in the end'}).text) 
+# pprint(cli.get('/todo-1', **hxhdr).text) # same to what displayed in the browser
 
 
 serve()
